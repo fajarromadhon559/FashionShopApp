f4d06a8640f3ed806d485e2b87e9baf0e0acb68b
fajarromadhon559 <fajarromadhon559@gmail.com>
2023-10-03 08:36:04 +0700

    update 10/3/23

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -4,9 +4,14 @@ import 'package:sub_flutter/widget/bottom_widget.dart';
 import 'package:sub_flutter/widget/widget_collection.dart';
 import 'package:sub_flutter/widget/widget_other_fashion.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
